d7ab0f67ce0af9e4944ef431484ef3fe0ba4c081
xVc323 <41209493+xVc323@users.noreply.github.com>
2024-12-06 22:22:50 +0100

    error handler

--- convert/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('uploadForm').onsubmit = function(event) {
-    event.preventDefault();
-
-    const formData = new FormData(this);
-    window.location.href = '/convert2.html';
-};
